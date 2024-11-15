0c78baff3f2fdc995e353fe71baff0eb608a3e7b
unknown <jonasojedaramirez@alumno.ieselrincon.es>
2024-11-15 13:12:10 +0000

    login

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Exit',
+      url: '/salir',
+      icon: 'exit'
+    }
+  ];
+
+  token = 'adfrkerqerty ';
+
+
+  constructor(
+    private router: Router
+  ) {}
+    
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login(){
+    localStorage.setItem('token', this.token);
+    this.router.navigate(["/home"]);
+  }
 }
